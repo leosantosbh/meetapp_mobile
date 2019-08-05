838db556f1580e1c97c298ccd11daf3f5786a93e
leosantosbh <soundsystem07@gmail.com>
2019-08-04 23:05:10 -0300

    meetapp mobile

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
+
 import { Provider } from 'react-redux';
 
 import '~/config/ReactotronConfig';
@@ -9,12 +12,39 @@ import App from './App';
 
 // import { Container } from './styles';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  );
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    OneSignal.init('0858ea61-919f-477c-ae93-b1aaf04d02a6');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
